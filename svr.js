f5e893b79f21cacc97251d029a0929fe663dc616
Ollie Green <72754829+UP936979@users.noreply.github.com>
2021-03-10 17:09:16 +0000

    check

--- svr.js
+++ svr.js
@@ -1,23 +1,11 @@
-// calls in the http module to allow the server to communicate to the server/client
-//defines the host of the server
-//defines the port the server will listen to
+const express = require('express')
+const app = express();
+const port = 8000;
 
-
-const http = require("http");
-const host = 'localhost';
-const port = 8080;
-
-//function will respond whenever someone accesses the server with string
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    res.end("potentially functioning server");
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
-/*
-1, creates a server object with http module's createServer function
-2, binds the server to listen to the port and host defined earlier
-*/
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
