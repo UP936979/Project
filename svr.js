4d5f69182e9133e4743b5a37b84a83d79243d239
UP943735 <72758747+UP943735@users.noreply.github.com>
2021-03-10 17:24:54 +0000

    POST

--- svr.js
+++ svr.js
@@ -3,9 +3,21 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+  <h1> Do you want to kiss me?</h1>
+  <form action = "/result" method ="POST">
+    <input type ="text">
+    <button>Submit Answer<button>
+  </form>
+  `)
 });
+
+app.post("/result", (req,res) => {
+    res.send("Thanks for Submitting the form.")
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
+
 });
